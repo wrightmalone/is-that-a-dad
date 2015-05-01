367127825a428a96ac7d55be0986557f9beca622
wrightmalone <wrightmalone@tnwinc.com>
2015-04-30 20:24:28 -0400

    create solution

---
+++ is-that-a-dad/is-that-a-dad-form/Form1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace is_that_a_dad_form
+{
+  public partial class Form1 : Form
+  {
+    public Form1() {
+      InitializeComponent();
+    }
+  }
+}
---
+++ is-that-a-dad/is-that-a-dad/GetAge.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace is_that_a_dad
+{
+    public class GetAge
+    {
+
+    }
+}
