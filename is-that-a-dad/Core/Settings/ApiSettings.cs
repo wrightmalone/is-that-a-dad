8cd25069da81d51de7c22969aec8a9208728117d
wrightmalone <wrightmalone@gmail.com>
2019-04-10 11:51:30 -0400

    Update ApiSettings.cs

--- is-that-a-dad/Core/Settings/ApiSettings.cs
+++ is-that-a-dad/Core/Settings/ApiSettings.cs
@@ -6,7 +6,7 @@ namespace is_that_a_dad.Core.Settings
   {
     public static Uri BaseUri = new Uri("http://api.cn.faceplusplus.com/");
     public static Uri TestUri = new Uri("http://localhost:9000/");
-    public static string ApiKey = "2affcadaeddd18f422375adc869f3991";
-    public static string ApiSecret = "EsU9hmgweuz8U-nwv6s4JP-9AJt64vhz";
+    public static string ApiKey = "";
+    public static string ApiSecret = "";
   }
 }
